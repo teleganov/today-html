b6b4f52a665fb7b1213179704d091ef10cb40f05
Yevgeniy Teleganov <teleganov@me.com>
2016-01-06 00:29:23 -0500

    auto-generating calendar complete

--- scripts.js
+++ scripts.js
@@ -16,14 +16,97 @@ var icons = {
   '13d': 'wi-snow',
   '13n': 'wi-snow',
   '50d': 'wi-fog',
-  '50n': 'wi-fog',
-
-}
+  '50n': 'wi-fog'
+};
 var dateString = moment().format("MMMM Do, YYYY");
 var dayString  = moment().format("dddd");
 var hourString = moment().format("hh");
 var minuteString = moment().format("mm A");
+var currentDay;
 
+var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+if(moment().isLeapYear()) daysInMonth[1] = 29;
+
+var updateCalendar = function(){
+  var today = moment();
+  if(today.format("D") === currentDay) return; // don't do all this work if the day has not changed.
+  currentDay = today.format("D");
+  var todayDate = parseInt(today.format('D'));
+  var month = parseInt(today.format('M')) - 1;
+  var firstDayOfMonth = moment(today.format('YYYY-MM'))
+  var prevMonth = month - 1;
+  var nextMonth = month + 1;
+  if(prevMonth < 0) prevMonth += 12;
+  if(nextMonth > 11) nextMonth -= 12;
+
+  var firstDayOfWeek = parseInt(firstDayOfMonth.format('d'));
+
+  var dayLayout = [
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0]
+  ];
+
+  var prevDayCounter = daysInMonth[prevMonth];
+  var currDayCounter = 1;
+  var weekCounter = 0, weekDay = 0;
+  var todayLocation = [0, 0];
+  var nextMonthLocation = [0, 0];
+
+  // fill in the first week
+  for(var a = firstDayOfWeek - 1; a >= 0; a--) {
+    dayLayout[0][a] = prevDayCounter;
+    prevDayCounter--;
+  }
+  for(var b = firstDayOfWeek; b < 7; b++) {
+    dayLayout[0][b] = currDayCounter;
+    if(currDayCounter === todayDate) todayLocation = [weekCounter, weekDay];
+    currDayCounter++;
+  }
+
+  weekCounter = 1;
+  // fill in the second through last week of current month
+  for(var c = currDayCounter; c < daysInMonth[month] + 1; c++) {
+    dayLayout[weekCounter][weekDay] = c;
+    currDayCounter = c;
+    if(currDayCounter === todayDate) todayLocation = [weekCounter, weekDay];
+    weekDay++;
+    if(weekDay > 6){
+      weekDay = 0;
+      weekCounter++;
+    }
+  }
+
+  // fill in remainder of next month
+  nextMonthLocation = [weekCounter, weekDay];
+  for(var d = 1; d < daysInMonth[nextMonth] + 1; d++) {
+    dayLayout[weekCounter][weekDay] = d;
+    weekDay++;
+    if(weekDay > 6){
+      weekDay = 0;
+      weekCounter++;
+    }
+    if(weekCounter > 5) break;
+  }
+
+  var enableGreying = false;
+
+  var rows = $('#calendar-container').find('.calendar-row');
+  for(var i = 0; i < rows.length; i++) {
+    var row = rows[i];
+    for(var k = 1; k < 8; k++) {
+      var identifier = '.calendar-row-td-' + k;
+      var td = $(row).find(identifier);
+      td.text(dayLayout[i][k-1]);
+      if(i == 0 && k-1 < firstDayOfWeek) td.attr('class', identifier + ' greyed');
+      if(todayLocation[0] == i && todayLocation[1] == k-1) td.attr('class', identifier + ' today');
+      if(i > nextMonthLocation[0] || ( i == nextMonthLocation[0] && k-1 >= nextMonthLocation[1])) td.attr('class', identifier + ' greyed');
+    }
+  }
+}
 
 var updateTime = function(){
   dateString = moment().format("MMMM Do, YYYY");
@@ -82,7 +165,9 @@ var updateWeather = function(){
 $(document).ready(function(){
   // Date and Time
   updateTime();
+  updateCalendar();
   setInterval(updateTime, 500);
+  setInterval(updateCalendar, 60000); // check every minute
 
   // Weather
   OWM.init(APIkey, 'imperial');
